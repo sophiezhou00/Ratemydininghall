0f36107511e4cb0130271808f9c112377ec10640
jellyuan123 <jyuanschaber@gmail.com>
2025-02-22 16:16:36 -0500

    updated tailwind stuff
    made the bagkround light blue, updated and connected the files

---
+++ app/dewick-page/page.tsx
@@ -0,0 +1,9 @@
+export default function DewickPage() {
+        return (
+          <main>
+            <h1>Dewick Page</h1>
+            <p>Email us at dewick YAYYY</p>
+          </main>
+        );
+      }
+      
