58686ed0304a91966ca942f7ff79c51bd4a4b680
jellyuan123 <jyuanschaber@gmail.com>
2025-02-22 15:22:28 -0500

    npmrundev-working

---
+++ app/frontend/testing.tsx
@@ -0,0 +1,9 @@
+// // app/page.tsx
+// export default function Home() {
+//         return (
+//           <main className="flex min-h-screen items-center justify-center bg-gray-100">
+//             <h1 className="text-3xl font-bold">Welcome to My Next.js App!</h1>
+//           </main>
+//         );
+//       }
+      
