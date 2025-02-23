ef8cc2fe92eee186663119b0d3acc9b3d4de417b
sophiezhou00 <sophiezhou00@gmail.com>
2025-02-22 21:25:34 -0500

    added avg daily score

--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,10 @@ import Image from "next/image";
 const DiningHallButton = ({ name, score }: { name: string; score: number }) => {
   return (
     <button className="w-[850px] h-32 text-4xl font-bold text-white bg-[#599CDF] rounded-lg shadow-lg hover:bg-blue-800 flex items-center space-x-10">
-      {/* Dynamic Score Box */}
-      <div className="w-16 h-16 bg-gray-300 text-black text-2xl flex items-center justify-center ml-10 mr-6">
-        {score}
+      {/* Dynamic Score Box with Label Below */}
+      <div className="w-20 h-20 bg-gray-300 text-black flex flex-col items-center justify-center ml-10 mr-6 rounded-md">
+        <span className="text-3xl font-bold">{score}</span>
+        <span className="text-xs font-semibold text-black ">Avg Daily Score</span>
       </div>
       <span>{name}</span>
     </button>
@@ -19,14 +20,13 @@ export default function HomePage() {
   return (
     <div className="min-h-screen bg-[#C9E6FF] flex flex-col">
       {/* Logo and "Tufts University" text next to each other */}
-      <div className="flex items-center space-x-4  ml-40  mt-10">
+      <div className="flex items-center space-x-4 ml-40 mt-10">
         <Image src="/images/Elephant.png" width={120} height={100} alt="Tufts Logo" />
         <p className="text-6xl font-bold text-black">Tufts University</p>
       </div>
 
-       {/* Large Underline */}
-       <div className="mx-auto translate-x-[-50px] w-[50%] h-1 bg-[#1E3A5F] "></div>
-
+      {/* Large Underline */}
+      <div className="mx-auto translate-x-[-50px] w-[50%] h-1 bg-[#1E3A5F] "></div>
 
       {/* Two Large Wide Buttons */}
       <div className="flex mt-10 flex-col items-center space-y-10 w-full px-8">
